3b519d53c77a63672ee2346c6910ed4d95ff2fb0
Fardus Ahmed <fardus11@gmail.com>
2023-06-11 15:26:17 -0400

    added lofi tracks

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import AudioPlayer from "./AudioPlayer";
 import TimeDisplay from "./TimeDisplay";
 
-
 export default function App() {
-	const audioSources = ["deepwater.mp3", "salty sailor.mp3"];
+	const audioSources = [
+		"Cozy-Place-Chill-Background-Music.mp3",
+		"Ghostrifter-Official-City-Lights.mp3",
+		"Ghostrifter-Official-Lost-In-Thought.mp3",
+		"Heart-Of-The-Ocean.mp3",
+		"Late-at-Night.mp3",
+		"Morning-Routine-Lofi-Study-Music.mp3",
+		"purrple-cat-green-tea.mp3",
+		"roa-music-walk-around-lofi-version.mp3",
+		"Serendipity.mp3",
+		"Still-Awake-Lofi-Study-Music.mp3",
+		"storm-clouds-purpple-cat.mp3",
+		"Winter-Night.mp3",
+	];
 	return (
 		<div className="container">
-
 			<video className="background-video" autoPlay loop muted playsInline>
 				<source src="/outside.mp4" type="video/mp4" />
 			</video>
