de755d967bf9a6432fb5f4413277ab9e5e1542fa
Fardus Ahmed <fardus11@gmail.com>
2023-06-11 15:31:50 -0400

    Works

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,18 @@ import TimeDisplay from "./TimeDisplay";
 
 export default function App() {
 	const audioSources = [
-		"Cozy-Place-Chill-Background-Music.mp3",
-		"Ghostrifter-Official-City-Lights.mp3",
-		"Ghostrifter-Official-Lost-In-Thought.mp3",
-		"Heart-Of-The-Ocean.mp3",
-		"Late-at-Night.mp3",
-		"Morning-Routine-Lofi-Study-Music.mp3",
-		"purrple-cat-green-tea.mp3",
-		"roa-music-walk-around-lofi-version.mp3",
-		"Serendipity.mp3",
-		"Still-Awake-Lofi-Study-Music.mp3",
-		"storm-clouds-purpple-cat.mp3",
-		"Winter-Night.mp3",
+		"lofi/Cozy-Place-Chill-Background-Music.mp3",
+		"lofi/Ghostrifter-Official-City-Lights.mp3",
+		"lofi/Ghostrifter-Official-Lost-In-Thought.mp3",
+		"lofi/Heart-Of-The-Ocean.mp3",
+		"lofi/Late-at-Night.mp3",
+		"lofi/Morning-Routine-Lofi-Study-Music.mp3",
+		"lofi/purrple-cat-green-tea.mp3",
+		"lofi/roa-music-walk-around-lofi-version.mp3",
+		"lofi/Serendipity.mp3",
+		"lofi/Still-Awake-Lofi-Study-Music.mp3",
+		"lofi/storm-clouds-purpple-cat.mp3",
+		"lofi/Winter-Night.mp3",
 	];
 	return (
 		<div className="container">
